862526066eac659147122e3b9da64eaff39a2ad4
Alexandre Bouras <alexandre.bouras.1@ulaval.ca>
2026-01-07 10:22:42 -0500

    bigram language model

---
+++ makemore.py
@@ -0,0 +1,67 @@
+"""
+you give this script some words (one per line) and it will generate more things like it.
+uses super state of the art Transformer AI tech
+this code is intended to be super hackable. tune it to your needs.
+
+Changes from minGPT:
+- I removed the from_pretrained function where we init with GPT2 weights
+- I removed dropout layers because the models we train here are small,
+  it's not necessary to understand at this stage and at this scale.
+- I removed weight decay and all of the complexity around what parameters are
+  and are not weight decayed. I don't believe this should make a massive
+  difference at the scale that we operate on here.
+"""
+
+import os
+import sys
+import time
+import math
+import argparse
+from dataclasses import dataclass
+from typing import List
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+
+
+
+# -----------------------------------------------------------------------------
+# Bigram language model
+
+class Bigram(nn.Module):
+    """
+    Bigram Language Model 'neural net'. 
+    Simply a lookup table of logits for the
+    next character given a previous character.
+    """
+
+    def __init__(self, config):
+        super().__init__()
+        n = config.vocab_size
+        self.logits = nn.Parameter(torch.zeros((n, n)))
+
+    def get_block_size(self):
+        return 1 # this model only needs one previous character to predict the next
+
+    def forward(self, idx, targets=None):
+
+         # 'forward pass'
+        logits = self.logits[idx]
+
+        # if we are given some desired targets also calculate the loss
+        loss = None
+        if targets is not None:
+            loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1), ignore_index=-1)
+
+        return logits, loss
+
+# -----------------------------------------------------------------------------
+
+
+
+
+
